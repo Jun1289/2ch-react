3b630287c54cb6622ee400c123aac111065abc27
Junpei <jun@note-Computer.local>
2023-10-01 16:57:48 +0900

    chore: frontend ディレクトリの作成とファイルの移動

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
