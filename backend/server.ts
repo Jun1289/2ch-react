d1ef2c6f8685c241a01daefbcf0c19ff308f5447
Junpei <jun@note-Computer.local>
2023-10-08 08:39:58 +0900

    chore: スレッド作成日時の日本時間への変更

--- backend/server.ts
+++ backend/server.ts
@@ -10,25 +10,31 @@ server.use(jsonServer.bodyParser)
 server.post('/threads/:threadId/comments', (req, res, next) => {
   const threadId = parseInt(req.params.threadId, 10);
   if (isNaN(threadId)) {
-    return res.status(400).send('Invalid thread ID');
+    return res.status(400).send('無効なスレッドIDです');
   }
+
   const comments = router.db.get('comments');
   const totalComments = comments.size().value();
+
   if (totalComments >= 10) {
     return res.status(401).json({
       message: 'スレッドのコメントは10個までです'
     })
   }
+
   if (!req.body.commentContent) {
     return res.status(401).json({
       message: 'コメントが入力されていません'
     })
   }
+
   if (!req.body.responder) {
     req.body.responder = '名無し'
   }
+
   const now = new Date().toISOString()
   req.body.createdAt = now
+
   next()
 })
 
@@ -38,22 +44,31 @@ server.post('/threads', (req, res, next) => {
       message: 'スレッドのタイトルが入力されていません'
     })
   }
+
   if (!req.body.topic) {
     return res.status(401).json({
       message: 'スレッドのトピックが入力されていません'
     })
   }
-  req.body.createdAt = Date.now()
+
+  const now = new Date().toISOString()
+  req.body.createdAt = now
 
   if (!req.body.builder) {
     req.body.builder = '名無し'
   }
+
   next()
 })
 
 server.delete('/clear-comments', (req, res) => {
   router.db.get('comments').remove().write();
   res.status(200).send('すべてのコメントを削除しました');
+});
+
+server.delete('/clear-threads', (req, res) => {
+  router.db.get('threads').remove().write();
+  res.status(200).send('すべてのスレッドを削除しました');
 });
 
 server.delete('/threads/:threadId/comments', (req, res) => {
