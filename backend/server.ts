a0830a4ddf6fd107c76c8b4d2d46c1c262bb9503
Junpei <jun@note-Computer.local>
2023-10-08 16:33:11 +0900

    chore: ユーザ新規登録とログインのバックエンド実装

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config()
+// import * as jsonServer from 'json-server'
 const jsonServer = require('json-server')
 // const serverMiddleware = require('./middleware.js')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt')
+const nfetch = require('node-fetch')
 
 server.use(middlewares)
 
@@ -11,15 +17,14 @@ server.use(jsonServer.bodyParser)
 // コメント投稿
 server.post('/threads/:threadId/comments', (req, res, next) => {
   const threadId = req.params.threadId;
-  if (!threadId || isNaN(Number(threadId))) {
+  if (isNaN(Number(threadId))) {
     return res.status(400).json({
       message: '無効なスレッドIDです'
     })
   }
 
-  const comments = router.db.get('comments');
+  const comments = router.db.get('comments') as any;
   const commentsForThread = comments.filter({ threadId }).size().value();
-  console.log(commentsForThread)
   if (commentsForThread >= 10) {
     return res.status(400).json({
       message: 'スレッドのコメントは10個までです'
@@ -68,19 +73,100 @@ server.post('/threads', (req, res, next) => {
 
 // お気に入りに登録
 
+
 // お気に入りから削除
 
+
+// ユーザ新規登録
+server.post("/users/register", async (req, res) => {
+  try {
+    const { name, password } = req.body;
+
+    const usersExist = await nfetch("http://localhost:8000/users")
+    const users = await usersExist.json()
+    const user = users.find((user) => name === user.name)
+
+    if (user) {
+      return res.status(400).json({ message: "既に使用されているユーザ名です。他のユーザ名を入力してください" })
+    }
+
+    // jwt.sign の第一引数は トークンのペイロード（ユーザの認証情報）、第二引数は秘密鍵の情報、第三引数はオプション（今回はトークンの有効期限を10分に設定）
+    const token = jwt.sign({ name }, process.env.ACCESS_TOKEN_SELECT, {
+      expiresIn: "10m",
+    })
+    // bcrypt.hash の第二引数は salt rounds(ハッシュ化を行う回数)
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const response = await nfetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        hashedPassword,
+        token
+      }),
+    });
+
+    const newUser = await response.json();
+
+    res.cookie("token", token, { httpOnly: true })
+
+    res.status(200).json({ message: "新しくユーザが作成されました", newUser })
+  } catch (error) {
+    console.log(error)
+    res.status(405).json(error);
+  }
+});
+
 // ログイン
+server.post("/users/signin", async (req, res) => {
+  try {
+    const inputUserName = req.body.name
+    const inputPassword = req.body.password
+    const usersExist = await nfetch("http://localhost:8000/users")
+    const users = await usersExist.json()
+    const user = users.find((user) => inputUserName == user.name)
+    const match = await bcrypt.compare(inputPassword, user.hashedPassword)
+
+    if (!user && !match) {
+      return res.status(400).json({ message: "ユーザ名とパスワードが間違っています" })
+    }
+    if (!user) {
+      return res.status(400).json({ message: "ユーザ名が間違っています" })
+    }
+    if (!match) {
+      return res.status(400).json({ message: "パスワードが間違っています" })
+    }
+
+    const token = jwt.sign(
+      { inputUserName },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "10m", }
+    )
+
+    res.status(200).json({
+      message: "ログインに成功しました",
+      ...user,
+      newToken: token,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json(error)
+  }
+})
 
 // ログアウト
 
+
 server.delete('/clear-comments', (req, res) => {
-  router.db.get('comments').remove().write();
+  (router.db.get('comments') as any).remove().write();
   res.status(200).send('すべてのコメントを削除しました');
 });
 
 server.delete('/clear-threads', (req, res) => {
-  router.db.get('threads').remove().write();
+  (router.db.get('threads') as any).remove().write();
   res.status(200).send('すべてのスレッドを削除しました');
 });
 
@@ -90,7 +176,7 @@ server.delete('/threads/:threadId/comments', (req, res) => {
     return res.status(400).send('Invalid thread ID');
   }
 
-  router.db.get('comments').remove({ threadId: threadId }).write();
+  (router.db.get('comments') as any).remove({ threadId: threadId }).write();
   res.status(200).send(`スレッドID ${threadId} のスレッドの子マントを全て削除しました`);
 });
 
