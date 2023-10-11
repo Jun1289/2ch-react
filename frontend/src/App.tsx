8e901ae36e80acd0a9dfca8c7e81c4c62204afd8
Junpei <jun@note-Computer.local>
2023-10-11 21:35:03 +0900

    test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home';
+// import { Home } from './components/Home';
 import { Header } from './components/Header';
 import { Thread } from './components/Thread';
 import "./styles/global.css"
