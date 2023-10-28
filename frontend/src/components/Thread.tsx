641bde3925d1364a9f6e3a57cb8c56a87c45aa53
Junpei <junpei.ozuka@glucose.jp>
2023-10-28 13:45:54 +0900

    frontend-feat: コメント投稿・取得・削除処理を useState から useReducer に変更

--- frontend/src/components/Thread.tsx
+++ frontend/src/components/Thread.tsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // import { CommentForm } from "./CommentForm";
+import { useReducer } from "react";
 
 type Thread = {
   id: number,
@@ -23,21 +24,90 @@ type Comment = {
   threadId: number
 }
 
+type CommentsState = {
+  comments: Comment[],
+  commentsIsLoading: boolean,
+  error: null | string
+}
+
+type Action =
+  | {
+    type: "delete_comment";
+    commentId: number;
+  }
+  | {
+    type: "add_comment";
+    newComment: Comment;
+  }
+  | {
+    type: "set_comments";
+    comments: Comment[];
+  }
+  | {
+    type: "set_error";
+    error: string | null;
+  }
+
+
+
+const commentReducer = (commentsState: CommentsState, action: Action) => {
+  // const { comments, commentsIsLoading, error } = commentsState
+  switch (action.type) {
+    case 'delete_comment': {
+      console.log("commentsData", action.commentId)
+      const newCommentsData = commentsState.comments?.filter((comment) => {
+        return comment.id !== action.commentId
+      }) || null;
+      console.log("newCommentsData", newCommentsData)
+      return {
+        ...commentsState,
+        comments: [...newCommentsData]
+      }
+      break;
+    }
+    case 'add_comment':
+      return {
+        ...commentsState,
+        comments: [...commentsState.comments, action.newComment],
+        error: null
+      }
+      break;
+    case 'set_comments':
+      console.log("payload", action.comments)
+      return {
+        ...commentsState,
+        comments: action.comments,
+        commentsIsLoading: false,
+        error: null
+      }
+      break;
+    case 'set_error':
+      console.log("error", action.error)
+      return {
+        ...commentsState,
+        error: action.error
+      }
+      break;
+  }
+}
+
+const commentsInitialState: CommentsState = {
+  comments: [],
+  commentsIsLoading: true,
+  error: null
+
+}
 
 export const Thread = () => {
   const { threadId } = useParams()
-  const [commentsData, setCommentsData] = useState<null | Comment[]>(null);
   const [threadData, setThreadData] = useState<null | Thread>(null);
   const [inputError, setInputError] = React.useState<null | string>(null);
   const formRef = React.useRef<HTMLFormElement>(null);
   const commentResponderRef = React.useRef<HTMLInputElement>(null)
   const commentContentRef = React.useRef<HTMLTextAreaElement>(null)
-  const [loadingComment, setLoadingComment] = useState(true)
   const [loadingThread, setLoadingThread] = useState(true)
 
-  const addNewComment = (newComment: Comment) => {
-    setCommentsData(prevComments => [...(prevComments || []), newComment]);
-  }
+  const [commentsState, commentDispatch] = useReducer(commentReducer, commentsInitialState);
 
   const formatDateTime = (dateString: string) => {
     const dateObj = new Date(dateString);
@@ -54,19 +124,13 @@ export const Thread = () => {
   const handleDelete = async (commentId: number, event: React.MouseEvent<Element, MouseEvent>) => {
     event.preventDefault();
     console.log(commentId)
-
     try {
       await axios.delete(`http://localhost:8000/comments/${commentId}`)
-        .then(function (response) {
-          console.log(response.data)
-          const newCommentsData = commentsData?.filter((comment) => {
-            return comment.id !== commentId
-          }) || null;
-          console.log(newCommentsData)
-          setCommentsData(newCommentsData)
+        .then(() => {
+          commentDispatch({ type: 'delete_comment', commentId })
         })
-    } catch (error) {
-      console.error("コメントの削除でエラーが発生しました:", error);
+    } catch (error: unknown) {
+      commentDispatch({ type: 'set_error', error: `コメントの削除中にエラーが起きました。${error}` })
     }
   }
 
@@ -84,15 +148,14 @@ export const Thread = () => {
         responder: commentResponderRef.current?.value,
         commentContent: commentContentRef.current?.value,
       })
-      console.log(response.data)
-      addNewComment(response.data)
+      commentDispatch({ type: 'add_comment', newComment: response.data })
       formRef.current?.reset()
     } catch (error) {
-      console.error("新しいスレッド作成時にエラーが発生しました", error);
+      commentDispatch({ type: 'set_error', error: `コメント投稿時にエラーが起きました。${error}` })
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [addNewComment])
+  }, [])
 
   // スレッドデータの取得
   useEffect(() => {
@@ -116,11 +179,11 @@ export const Thread = () => {
     const fetchData = async () => {
       try {
         const commentsData = await axios.get(`http://localhost:8000/threads/${threadId}/comments`)
-        setCommentsData(commentsData.data);
-        setLoadingComment(false)
+        commentDispatch({ type: 'set_comments', comments: commentsData.data })
+        // setLoadingComment(false)
       } catch (error) {
         console.error("コメントの取得でエラーが発生しました:", error);
-        setLoadingComment(false)
+        // setLoadingComment(false)
       }
     };
     fetchData();
@@ -129,7 +192,7 @@ export const Thread = () => {
 
   return (
     <>{
-      (loadingThread || loadingComment) ? (
+      (loadingThread || commentsState.commentsIsLoading) ? (
         null
       ) : (
         <div className="thread-wrapper content-wrapper">
@@ -138,9 +201,11 @@ export const Thread = () => {
             <p>{threadData?.topic}</p>
           </section>
           <section>
-            {commentsData ? (
+            {(commentsState.comments && commentsState.comments.length != 0) ? (
+              console.log("commentsState", commentsState),
+              console.log("commentsState.length", commentsState.comments.length),
               <ul>
-                {commentsData.map((comment) => (
+                {commentsState.comments.map((comment) => (
                   <li key={comment.id}>
                     <div>
                       <span>{comment.commentNo}: {comment.responder} : {formatDateTime(comment.createdAt)}</span>
